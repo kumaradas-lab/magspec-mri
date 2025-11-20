cbcef5caf615ec6c4e80a7fc3b261c6ddaed06a5
sdem-tmu <sdemircan@torontomu.ca>
2025-11-20 15:11:50 -0500

    debugging

--- StandardFunctions/HWFunctions/checkOpenMatlabUpdate.m
+++ StandardFunctions/HWFunctions/checkOpenMatlabUpdate.m
@@ -1,28 +1,4 @@
-function isUpdating = checkOpenMatlabUpdate(mySave, forceUpdate, feedback)
-%% Check for new version of OpenMatlab toolbox on PD server
-%
-%     isUpdating = checkOpenMatlabUpdate(mySave, forceUpdate, feedback)
-%
-% INPUT:
-%
-%   mySave        The mySave structure as created when running LoadSystem.
-%                 If the update check should be skipped during start-up, set the
-%                 following value prior to running LoadSystem:
-%                     mySave.checkUpdate = false;
-%
-%   forceUpdate   Check for update NOT taking the time of the last check into
-%                 account. (Default: true)
-%
-%   feedback      Show feedback in message boxes. (Default: false)
-%
-% ------------------------------------------------------------------------------
-% (C) Copyright 2017-2021 Pure Devices GmbH, Wuerzburg, Germany
-% www.pure-devices.com
-% ------------------------------------------------------------------------------
-
+function isUpdating = checkOpenMatlabUpdate(varargin)
+% Disabled update check due to MATLAB 2024/25 JavaFrame removal
+isUpdating = false;
 end
-
-
-%#function findjobj
-%#function isemptyfield
-
